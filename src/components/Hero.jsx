059e820e4d58b336242629c7b512e557ce051680
OmkarLande <omkarrlande.534@gmail.com>
2023-09-18 22:14:19 +0530

    initializing

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className='w-full flex justify-between items-center mb-10 pt-3'>
+            <img src={logo}  alt='sumz_logo' className='w-28 object-contain'/>
+            <button 
+                type='button'
+                onClick={()=> window.open('http://github.com/OmkarLande')}
+                className='black_btn'>
+                Github
+            </button>
+        </nav>
+
+        <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden'/>
+            <span className='orange_gradient'> 
+             OpenAl GPT-4
+            </span>
+        </h1>
+        <h2 className='desc'>
+        Simplify your rading with Summarize. an open source article
+        summarizer that transform lengthy articles into and
+        concise summaries.
+        </h2>
+    </header>
+  )
+}
+
+export default Hero
